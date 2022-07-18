3fcf2fe618928e706dea5bd012ca642c18af04e5
Marse9713 <raffaele.marseu@outlook.it>
2022-07-18 19:01:18 +0200

    quasi concluso l'es 2 di 3

--- esercizi_haskell/es3.hs
+++ esercizi_haskell/es3.hs
@@ -6,14 +6,16 @@ calcola se è ben formata, altrimenti restituisce (-1,-1)
 matrix_dim l = matrix_dm1 l (length l)
 
 
-matrix_dm1 l rows = if(rows == 0)
-    then (length l, length  l)
-    else if (length l == 0)
-        then (-1, -1)
-        else (matrix_dm2 l rows (length (head l)) 1 (tail l))
+matrix_dm1 l rows = 
+    if(rows == 0)
+        then (length l, length  l)
+        else if (length l == 0)
+            then (-1, -1)
+            else (matrix_dm2 l rows (length (head l)) 1 (tail l))
 
 
-matrix_dm2 l rows c i l1 = if(rows == i)
+matrix_dm2 l rows c i l1 = 
+    if(rows == i)
     then (matrix_dm1 l 0)
     else if (length (head l1) /= c)
         then (matrix_dm1 [] rows)
@@ -21,3 +23,21 @@ matrix_dm2 l rows c i l1 = if(rows == i)
 
 testmatrice = matrix_dim [[0, 8, 8, 9], [4, 3, 2, 2], [2, 3, 1, 7]]
 testmatricerror = matrix_dim [[0, 8, 8, 9, 1], [1, 4, 3, 2, 2], [2, 3, 1, 7]]
+
+
+{-Scrivere una funzione che calcoli una funzione colsums
+che somma tutti i valori delle singole colonne-}
+
+colsums l = colsums1 l 0 0
+
+colsums1 l r c =
+    if(r == (length (head l)))
+        then 
+        else (sumteste l c r) + (colsums1 l (r + 1) c)
+
+sumteste l c r = 
+    if(c == length l)
+    then 0
+    else (((l !! c) !! r) + (sumteste l (c + 1) r)) --return di un int
+
+testhead = colsums [[0, 8, 8, 9], [4, 3, 2, 2], [2, 3, 1, 7]]
