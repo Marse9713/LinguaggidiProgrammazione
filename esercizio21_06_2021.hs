42ab480f02e3e54401450fab83c297e39d769534
Marse9713 <raffaele.marseu@outlook.it>
2022-07-24 15:00:43 +0200

    continuazione esami

--- esercizio21_06_2021.hs
+++ esercizio21_06_2021.hs
@@ -1,3 +1,5 @@
+import Main (matrixcheck)
+import Distribution.Simple.Setup (falseArg)
 --check stringa palindroma
 
 checkpalindroma l = checkpalindroma1 l (reverse l)
@@ -23,5 +25,18 @@ testmat = checkmatrix [[1,2,2,1], [2,3,3,2], [4,1,1,4]]
 
 --palindroma rispetto a verticale (colonne palindrome)
 
-bringhead l = (head l)
+bringhead l = (head (head l))
+bringhead1 l = (head (tail l))
+
+colcheck l aux =
+    if()
+
+matixcheckor l = matrixcheckor1 l l []
+
+matrixcheckor1 l l1 aux =
+    if(length l1 == 0)
+        then True
+        else if (colcheck l1 aux)
+            then matrixcheckor1 l l1 aux
+            else False
 
