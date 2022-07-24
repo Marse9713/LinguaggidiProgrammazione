22981602b18c1be3043f2a64c603601f2932e5c7
Marse9713 <you@example.com>
2022-07-24 13:58:09 +0200

    aggiunta esercizi mancanti nelle varie sezioni + introduzione es 4

--- esercizi_haskell/es2.hs
+++ esercizi_haskell/es2.hs
@@ -1,3 +1,62 @@
 {-scrivere una funzione che data una lista ne costruis
 una rimuovendo gli elementi di posizione pari-}
 
+rimuovipari l = rimuovipari1 l 1 []
+
+rimuovipari1 l i aux = 
+    if(length l == 0)
+        then (reverse aux)
+        else if (i == 1)
+            then (rimuovipari1 (tail l) 2 ((head l):aux))
+            else (rimuovipari1 (tail l) 1 aux)
+
+testrimuovipari = rimuovipari [1, 2, 3, 4, 5, 6]
+
+{-scrivere una funzione che calcoli la somma degli elementi
+dispari della lista-}
+
+sumdispari l = sum1 (rimuovipari l) 0
+
+sum1 l ris = 
+    if(length l == 0)
+        then ris
+        else (sum1 (tail l) (ris + head l))
+
+testsumdispari = sumdispari [1, 2, 3, 4, 5, 6]
+
+{-quicksort polimorfo, quindi esegue l'ordinamento anche su stringhe o altro-}
+
+quicksort [] = []
+quicksort (x : xs) = 
+    quicksort [y | y <- xs, y < x] ++ x : quicksort [y | y <- xs, y >= x]
+
+testquick = quicksort [3, 1, 4, 2]
+testquick1 = quicksort ["mandi", "boiate", "avvinazato", "dioboe"]
+
+{-calcolo degli odd più piccoli (2)-}
+
+minOdd list = (couple (quicksort (filter odd list)) [])
+
+couple l aux =
+    if (length aux == 2)
+        then (reverse aux)
+        else couple (tail l) ((head l):aux)
+
+testmin = minOdd[8, 5, 7, 1, 4, 6, 9]
+
+{-lista di coppie-}
+
+coppia l = coppia1 l []
+
+coppia1 l aux =
+    if (length l == 0)
+        then (reverse aux)
+        else coppia1 (tail l) (((,) (head l) (couplecreate (tail l) 0 (head l))) : aux)
+
+couplecreate l s i =
+    if (length l == 0)
+        then s
+        else (couplecreate (tail l) (head l + s) i)
+
+
+testcpp = coppia [1,2,3,4]
--- esercizi_haskell/es3.hs
+++ esercizi_haskell/es3.hs
@@ -28,14 +28,12 @@ testmatricerror = matrix_dim [[0, 8, 8, 9, 1], [1, 4, 3, 2, 2], [2, 3, 1, 7]]
 {-Scrivere una funzione che calcoli una funzione colsums
 che somma tutti i valori delle singole colonne-}
 
-colsums l = colsums1 l 0 0
+colsums l = colsums1 l 0 0 []
 
-colsums1 l c r = colsums2 l c r []
-
-colsums2 l c r ris= 
+colsums1 l c r ris= 
     if(r == (length (head l)))
         then reverse ris
-        else (colsums2 l c (r + 1) ((sumteste l c r 0) ++ ris))
+        else (colsums1 l c (r + 1) ((sumteste l c r 0) ++ ris))
 
 sumteste l c r s = 
     if(c == length l)
@@ -43,3 +41,24 @@ sumteste l c r s =
     else (sumteste l (c + 1) r (((l !! c) !! r) + s))  -- + (sumteste l (c + 1) r) return di un int
 
 testhead = colsums [[0, 8, 8, 9], [4, 3, 2, 2], [2, 3, 1, 7]]
+
+
+{-colminmax per colonna-}
+--minmaxcol l = minmaxcol1 l (head l) (head (tail l))
+
+calcmin l min i=
+    if (i == length l - 1)
+        then min
+        else if (min <= (l !! i))
+            then (calcmin l min (i + 1))
+            else (calcmin l (l !! i) 0)
+
+{-minmaxcol1 l min max =
+    if (length l == 0)
+        then (min, max)
+        else if (min < max)
+            then (minmaxcol1 (tail l) min ())
+-}
+--testminmax = minmaxcol [[1, 2, 3, 4], [8, 2, 3, 5], [2, 5, 7, 8], [5, 7, 8, 9]]
+
+testminmax = calcmin [1, 2, 3, 4]
