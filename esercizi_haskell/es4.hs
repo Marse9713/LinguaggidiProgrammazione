436bb29c65cdb8b1a9ca9c05394906130047da20
Marse9713 <raffaele.marseu@outlook.it>
2022-09-28 08:36:39 +0200

    Varie correzioni sugli esercizi di haskell

--- esercizi_haskell/es4.hs
+++ esercizi_haskell/es4.hs
@@ -1,5 +1,5 @@
 --BST, definizione
-data BST a = Nulla | Node {
+data BST a = Null | Node {
 
     val::a,
 
@@ -10,18 +10,18 @@ data BST a = Nulla | Node {
     deriving (Eq, Ord, Read, Show)
 
 --base dell'albero per uso sugli esercizi
-t = (Node 10 (Node 6 (Node 3 Nulla (Node 9 Nulla Nulla)) (Node 7 Nulla Nulla)) (Node 20 Nulla (Node 29 Nulla Nulla)))
+t = (Node 10 (Node 6 (Node 3 Null (Node 9 Null Null)) (Node 7 Null Null)) (Node 20 Null (Node 29 Null Null)))
 
 {-1. calcolare la somma dei valori di un albero-}
 
-sommaalbero Nulla = 0 -- poniamo un valore 0 per avere il void
+sommaalbero Null = 0 -- poniamo un valore 0 per avere il void
 sommaalbero (Node r left right) = r + (sommaalbero left) + (sommaalbero right)
 
 testalberosomma = sommaalbero t
 
 {-2. calcolare la somma dei valori dispari di un albero-}
 
-sommadispari Nulla = 0
+sommadispari Null = 0
 sommadispari (Node r left right) = 
     if (odd r) --elimina i pari
         then r + sommadispari left + sommadispari right
@@ -31,7 +31,7 @@ testsommadispari = sommadispari t
 
 {-3. inserimento di un valore x in un albero t-}
 
-inserimento x Nulla = (Node x Nulla Nulla)
+inserimento x Null = (Node x Null Null)
 inserimento x (Node r left right) =
     if (x == r)
         then (Node r left right)
@@ -39,5 +39,5 @@ inserimento x (Node r left right) =
             then (Node r left (inserimento x right))
             else (Node r (inserimento x left) right)
 
-alberoesempio = (Node 7 (Node 5 Nulla Nulla) (Node 16 Nulla Nulla))
+alberoesempio = (Node 7 (Node 5 Null Null) (Node 16 Null Null))
 testinserimento = inserimento 2 alberoesempio
